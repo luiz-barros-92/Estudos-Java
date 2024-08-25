232aa614a17141088c8a56b8b67775181ed9dd42
Luiz Barros <lck.barros@gmail.com>
2024-08-25 01:12:53 -0300

    Update Idioma.java

--- Idioma do Sistema/Idioma.java
+++ Idioma do Sistema/Idioma.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class LinguagemDoSistema {
 
 	public static void main(String[] args) {
-		Locale loc = Locale.getDefault();
-		System.out.println("Seu sistema está em  " + loc.getDisplayLanguage(loc) + ".");
+		Locale loc = Locale.getDefault();							//captura o local
+		System.out.println("Seu sistema está em  " + loc.getDisplayLanguage(loc) + ".");	//printa a linguagem
 
 	}
 
